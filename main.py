8cbaa998f520ee4ca2b3c254d36572d6931ca049
KICC <948209147@qq.com>
2018-09-15 15:56:46 +0800

    提交测试的doc文件

---
+++ main.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# @Author: KICC
+# @Date:   2018-07-27 12:03:41
+# @Last Modified by:   KICC
+# @Last Modified time: 2018-08-11 17:31:30
+
+from docx import Document
+doc = Document()
+doc.add_paragragh('Hello')
