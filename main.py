48ca8f120501ea17b4d99b654ecbc9177c32ea7f
KICC <948209147@qq.com>
2018-09-15 20:34:03 +0800

    移除main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-# @Author: KICC
-# @Date:   2018-07-27 12:03:41
-# @Last Modified by:   KICC
-# @Last Modified time: 2018-08-11 17:31:30
-
-from docx import Document
-doc = Document()
-doc.add_paragragh('Hello')
