7bafd00766405b1b7c751e830d8deff2910abe9d
KICC <948209147@qq.com>
2018-09-18 16:33:31 +0800

    修改章\节标题的format

--- utils.py
+++ utils.py
@@ -2,11 +2,11 @@
 # @Author: KICC
 # @Date:   2018-09-16 17:34:46
 # @Last Modified by:   KICC
-# @Last Modified time: 2018-09-18 13:43:18
+# @Last Modified time: 2018-09-18 16:32:48
 
 from docx.shared import Inches, Pt
 from docx.oxml.ns import qn
-
+from docx.enum.text import WD_LINE_SPACING
 
 def delete_paragraph(paragraph):
     p = paragraph._element
@@ -32,10 +32,12 @@ def modify_character(back_text, p, category):
             else:
                 font.name = u'宋体'
                 run._element.rPr.rFonts.set(qn('w:eastAsia'), u'宋体')
-        elif category == '一级标题':
+        elif category == '章标题':
+            # 1 绪论
             pass
 
-        elif category == '二级标题':
+        elif category == '一级标题':
+            # 1.1
             pass
 
 
@@ -61,16 +63,38 @@ def adjust_paragraph_style(formats, paragraph, category):
     # 用category来确定段落格式的话, formats参数就没用了.
 
     if category == '正文':
+        # 标题以外的文字行距为"固定值"23磅,字符间距为"标准"
         p_format = paragraph.paragraph_format
         p_format.alignment = None
-        p_format.first_line_indent = Pt(-50)
+        p_format.first_line_indent = Pt(20)
         p_format.left_indent = Pt(21)
         p_format.right_indent = None
-        p_format.line_spacing = Pt(20)
+        p_format.line_spacing = Pt(23)
         p_format.space_before = None
 
-    elif category == '一级标题':
-        pass
+    elif category == '节标题':
+        # 1.1.1
+        # 节标题段前为0.5行, 段后为0.5行
+        p_format = paragraph.paragraph_format
+        p_format.alignment = None
+        p_format.first_line_indent = Pt(30)
+        p_format.left_indent = Pt(70)
+        p_format.right_indent = None
+        p_format.line_spacing_rule = WD_LINE_SPACING.SINGLE
+        p_format.line_spacing = 1.0
+        p_format.space_before = None
+
+    elif category=='章标题':
+        # 1.
+        # 章标题的段前0.8行,段后0.5行;
+        p_format = paragraph.paragraph_format
+        p_format.alignment = None
+        p_format.first_line_indent = Pt(30)
+        p_format.left_indent = Pt(50)
+        p_format.right_indent = None
+        p_format.line_spacing_rule = WD_LINE_SPACING.SINGLE
+        p_format.line_spacing = 1.3
+        p_format.space_before = None
 
     elif category == '摘要':
         pass
